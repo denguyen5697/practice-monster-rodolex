be7773affc84fd0f6940163ddb6f4f19b35fb414
unknown <ntdvt197@gmail.com>
2020-08-03 19:28:01 +0700

    Updates

---
+++ precache-manifest.b8e03123ee5d236f60858ece99f4f624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04993ebd959c172e163f6f1d1acabc01",
+    "url": "/monster-rodolex/index.html"
+  },
+  {
+    "revision": "0ee87c025b4d48e89c4a",
+    "url": "/monster-rodolex/static/css/main.bbccd4ba.chunk.css"
+  },
+  {
+    "revision": "1b62d5c56410c7498739",
+    "url": "/monster-rodolex/static/js/2.422b15d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rodolex/static/js/2.422b15d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee87c025b4d48e89c4a",
+    "url": "/monster-rodolex/static/js/main.c7000e6e.chunk.js"
+  },
+  {
+    "revision": "9ad79e4d345b731906e2",
+    "url": "/monster-rodolex/static/js/runtime-main.af849cd9.js"
+  }
+]);
